77e8ad4c4c162146a29c6081c2e4c9491a0422e9
maresilva <maresilva@users.noreply.github.com>
2026-07-09 21:16:58 -0300

    pagina soluções ok

--- replace_contato.ps1
+++ replace_contato.ps1
@@ -1,13 +1,13 @@
-$html = Get-Content -Path portfolio.html -Raw -Encoding UTF8
+$html = Get-Content -Path contato.html -Raw -Encoding UTF8
 $start = '<!-- MAIN CONTENT: SOLUCOES -->'
 $end = '</main>'
 $startIndex = $html.IndexOf($start)
 $endIndex = $html.IndexOf($end, $startIndex) + $end.Length
 
 if ($startIndex -ge 0 -and $endIndex -gt $startIndex) {
-    $newContent = Get-Content -Path portfolio_content.txt -Raw -Encoding UTF8
+    $newContent = Get-Content -Path contato_content.txt -Raw -Encoding UTF8
     $result = $html.Substring(0, $startIndex) + $newContent + $html.Substring($endIndex)
-    Set-Content -Path portfolio.html -Value $result -Encoding UTF8
+    Set-Content -Path contato.html -Value $result -Encoding UTF8
     Write-Host "Replaced successfully!"
 } else {
     Write-Host "Markers not found!"
---
+++ test_bounds.ps1
@@ -0,0 +1,11 @@
+﻿ = Get-Content -Path portfolio.html -Raw
+ = '<!-- MAIN CONTENT: SOLUCOES -->'
+ = '</main>'
+ = .IndexOf()
+ = .IndexOf(, ) + .Length
+
+if ( -ge 0 -and  -gt ) {
+    Write-Host "Found bounds!"
+} else {
+    Write-Host "Bounds not found!"
+}
